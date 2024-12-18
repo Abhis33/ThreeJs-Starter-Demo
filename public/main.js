fec67d6e3eab522dbe08236150e34f7269a7025c
Abhishek Mane <abhishek.mane33@gmail.com>
2024-12-18 11:29:42 -0800

    feat: Chain of skyboxes

--- public/main.js
+++ public/main.js
@@ -2,53 +2,75 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
 
+let currentSceneIndex = 0;
+let scenes = ['assets/1.jpg',
+  'assets/2.jpg',
+  'assets/3.jpg',
+  'assets/4.jpg',
+  'assets/5.jpg',
+  'assets/6.jpg',
+  'assets/7.jpg'
+]; // Array to store different 360-degree images - ASSETS folder is part og gitignore
+
 // Scene setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const loader = new THREE.TextureLoader();
 
-// Light Setup
-const light = new THREE.PointLight(0xffffff, 50)
-light.position.set(0.8, 1.4, 1.0)
-scene.add(light)
+// Load First Scene
+loadScene('assets/1.jpg');
 
-// Ambient Light Setup
-const ambientLight = new THREE.AmbientLight()
-scene.add(ambientLight)
+// Switching to next scene
+window.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowRight') { // Press the Right Arrow key
+        nextScene();
+    }
+});
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// const material = new THREE.MeshBasicMaterial( { color: 0x773399 } );
-// const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
-
-const fbxLoader = new FBXLoader()
-fbxLoader.load(
-    './assets/drone.fbx',
-    (object) => {
-        object.scale.set(.03, .03, .03)
-        scene.add(object)
-    },
-    (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-    },
-    (error) => {
-        console.log(error)
-    }
-)
-
 camera.position.z = 5;
 
 const controls = new OrbitControls( camera, renderer.domElement );
 
 function animate() {
-
-	// cube.rotation.x += 0.01;
-	// cube.rotation.y += 0.01;
-
 	renderer.render( scene, camera );
+}
 
+
+// Load a 360-degree image and add it to the scene
+function loadScene(imagePath) {
+  // OPTION 1 - Gives brighter result - SLOWER SPEED
+  // const texture = new THREE.TextureLoader().load(imagePath);
+  // texture.mapping = THREE.EquirectangularReflectionMapping;
+  //
+  // // Create a sphere to wrap the 360 image
+  // const geometry = new THREE.SphereGeometry(500, 60, 40);
+  // geometry.scale(-1, 1, 1); // Invert the sphere to look inside
+  //
+  // const material = new THREE.MeshBasicMaterial({
+  //   map: texture
+  // });
+  // const mesh = new THREE.Mesh(geometry, material);
+  //
+  // // Add the mesh to the scene
+  // scene.add(mesh);
+
+  // OPTION 2 - Gives realistic result - SLIGHTLY FASTER
+  const texture = loader.load(
+    imagePath,
+    () => {
+      texture.mapping = THREE.EquirectangularReflectionMapping;
+      texture.colorSpace = THREE.SRGBColorSpace;
+      scene.background = texture;
+    });
+}
+
+// Add navigation to switch between scenes
+function nextScene() {
+  currentSceneIndex = (currentSceneIndex + 1) % scenes.length;
+  loadScene(scenes[currentSceneIndex]);
 }
